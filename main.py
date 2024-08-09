b842444c9721faaa056d83c6ffcd17448519ad69
Elidallana Cristancho <cristanchodayana062017@gmail.com>
2024-08-09 00:15:13 -0500

    feat: :sparkles: Se agrego menu
    Agregue un menu donde colocaran el registro de las ventas, compras. Nos generara un informe de ventas y un stock de los productos
    
    @By ElidallanaCristancho

---
+++ main.py
@@ -0,0 +1,32 @@
+from gestion_ventas import registrar_venta
+from gestion_compras import registrar_compra
+from gestion_informes import generar_informe_ventas, generar_informe_stock
+
+def main():
+    while True:
+        print("\nSistema de Gestión PanCamp")
+        print("1. Registrar Venta")
+        print("2. Registrar Compra")
+        print("3. Generar Informe de Ventas")
+        print("4. Generar Informe de Stock")
+        print("5. Salir")
+        
+        opcion = input("Seleccione una opción: ")
+        
+        if opcion == '1':
+            registrar_venta()
+        elif opcion == '2':
+            registrar_compra()
+        elif opcion == '3':
+            generar_informe_ventas()
+        elif opcion == '4':
+            generar_informe_stock()
+        elif opcion == '5':
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    main()
+
